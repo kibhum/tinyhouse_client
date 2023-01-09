e9c9a40ddaf0f888104b59178282ffd1ab9f351e
kibethh <kibethh@gmail.com>
2023-01-09 10:49:42 +0300

    started listing

--- src/sections/Listing/index.tsx
+++ src/sections/Listing/index.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { LISTING } from "../../lib/graphql/queries";
+import { useParams } from "react-router-dom";
+import { Layout } from "antd";
+import {
+  Listing as ListingData,
+  ListingVariables,
+} from "../../lib/graphql/queries/Listing/__generated__/Listing";
+import { PageSkeleton, ErrorBanner } from "../../lib/components";
+
+const { Content } = Layout;
+
+const PAGE_LIMIT = 3;
 
 export const Listing = () => {
+  let { id } = useParams();
+  const [bookingsPage, SetBookingsPage] = useState(1);
+  const { loading, data, error } = useQuery<ListingData, ListingVariables>(
+    LISTING,
+    {
+      variables: {
+        id: id ? id : " ",
+        bookingsPage,
+        limit: PAGE_LIMIT,
+      },
+    }
+  );
+  if (loading) {
+    <Content className="listings">
+      <PageSkeleton />
+    </Content>;
+  }
+  if (error) {
+    <Content className="listings">
+      <ErrorBanner description="This listing may not exist or we've encountered an error. Please try again soon!" />
+    </Content>;
+  }
+
+  const listing = data ? data.listing : null;
+  const listingBookings = listing ? listing.bookings : null;
   return <h2>Listing</h2>;
 };
